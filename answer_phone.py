af3877dac8aee3165ded77312fd1f15fc88964c8
Kyle Liao <thekyleliao@gmail.com>
2024-05-29 17:57:07 -0400

    Add story 2

--- answer_phone.py
+++ answer_phone.py
@@ -5,7 +5,8 @@ import random
 app = Flask(__name__)
 
 story_dict = {
-    1: "https://timberwolf-cheetah-1758.twil.io/assets/story1.mp3"
+    1: "https://timberwolf-cheetah-1758.twil.io/assets/story1.mp3",
+    2: "https://timberwolf-cheetah-1758.twil.io/assets/story2.wav"
 }
 
 @app.route("/answer", methods=['GET', 'POST'])
